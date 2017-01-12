bcceb4c404277ec05b2bd5699cb2246236e19cec
William Doyle <williamdoyleiii@gmail.com>
2017-01-12 13:53:17 -0500

    added some basic framework for experiment testing need refinement

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
 
-echo "Running SafeSearch $1 25 iterations..."
+echo "Running SafeSearch $1 50 iterations..."
+echo "Safety Flag $2..."
 
-for i in {0..25}
+for i in {0..49}
 do
-  echo "Running $1 $i experiment..."
-  timeout 180 java -jar ../build/libs/SafeSearch-1.0-SNAPSHOT.jar -v -l -s < ../input/vehicle/vehicle$i.v > ../results/$1$i.results
+  echo "Running $1 $2 $i experiment..."
+  timeout 10 java -jar ../build/libs/SafeSearch-1.0-SNAPSHOT.jar $1 -l $2 < ../input/vehicle/vehicle$i.v > ../results/$1$2$i.results
 done
 
 echo "Finished!"
