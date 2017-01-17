c4fd4ae818e9089a65ce53363f0d8ff556a428a9
William Doyle <williamdoyleiii@gmail.com>
2017-01-17 01:57:07 -0500

    merge some more runs done

--- scripts/run_experiment.sh
+++ scripts/run_experiment.sh
@@ -4,7 +4,7 @@
 echo "Running SafeSearch $1 500 iterations..."
 echo "Safety Flag $2..."
 
-for i in {0..499}
+for i in {0..4}
 do
   echo "Running $1 $2 $i experiment..."
   timeout 10 java -jar ../build/libs/SafeSearch-1.0-SNAPSHOT.jar $1 -l $2 < ../input/vehicle/vehicle$i.v > ../results/$2$i.results
